52594a8f5425bc01af7a96b56d41244a4fbf32e7
Anurag Mittal <anurag.mittal@scality.com>
2020-10-15 16:45:04 +0200

    starting caching mongoDB with redis

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://localhost:27018,localhost:27019,localhost:27020/?w=majority&replicaSet=rs0&readPreference=primary',
   cookieKey: '123123123',
 };
