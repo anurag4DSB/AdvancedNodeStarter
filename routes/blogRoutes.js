b4d09d23475abcadd1b526c00b7f44f1b9956dba
Anurag Mittal <anurag.mittal@scality.com>
2020-10-16 02:05:21 +0200

    nested cache and middleware

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
-
 const Blog = mongoose.model('Blog');
+const cleanCache = require('../middlewares/cleanCache')
 
 module.exports = app => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
@@ -13,8 +13,10 @@ module.exports = app => {
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+  app.get('/api/blogs', requireLogin, cleanCache, async (req, res) => {
+    const blogs = await Blog
+      .find({ _user: req.user.id })
+      .cache({ key: req.user.id });
 
     res.send(blogs);
   });
